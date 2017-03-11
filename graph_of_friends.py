235e98fadd4842ae45ae5b431012c39346b408eb
abletoreinforce <abletoreinforce@yandex.ru>
2017-03-12 00:11:27 +0300

    Add files via upload

---
+++ graph_of_friends.py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt
+import networkx as nx
+import requests
+
+HOST = 'https://api.vk.com/method/'
+VERSION = '5.62'
+
+font = {'family': 'Verdana',
+        'weight': 'normal'}
+
+r = requests.get(HOST + 'friends.get', timeout = 100, params={'user_id': 264251957, 'fields': 'first_name', 'v': VERSION})
+
+G = nx.Graph()
+
+for person in r.json()['response']['items']:
+    G.add_node(person['last_name'] + ' ' + person['first_name'])
+    r2 = requests.get(HOST + 'friends.get', timeout = 100, params={'user_id': person['id'], 'fields':'first_name','v': VERSION})
+    for i in range(len(r2.json()['response']['items'])):
+        if r2.json()['response']['items'][i] in r.json()['response']['items']:
+            G.add_edge(r2.json()['response']['items'][i]['last_name'] + ' ' + r2.json()['response']['items'][i]['first_name'], person['last_name'] + ' ' + person['first_name'])
+
+
+nx.draw_networkx(G, edge_color='r', node_color='b', node_size = 20, alpha = 0.5, font_family = 'Verdana', font_size = 8)
+plt.axis('off')
+plt.show()
+
